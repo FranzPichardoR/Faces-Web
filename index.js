8a34d059fdcc195ecfd2fd7e394b946832aae64b
Franz Pichardo Rodriguez <franz.pichardo.r@gmail.com>
2020-11-02 23:41:37 -0400

    Added searchbyimage function

--- index.js
+++ index.js
@@ -10,22 +10,25 @@ AWS.config.update({ region: 'us-east-1' });
 
 var rekognition = new AWS.Rekognition();
 
-var params = {
-  CollectionId: "faces",
-  DetectionAttributes: [
-  ],
-  ExternalImageId: "edrekogbucketMarcMerill1.jpg", //TODO
-  Image: {
-    S3Object: {
-      Bucket: "edrekogbucket",
-      Name: "MarcMerill1.jpg"
+function searchByImage(image) {
+  var params = {
+    CollectionId: "faces",
+    DetectionAttributes: [
+    ],
+    Bytes: {
+      S3Object: {
+        Bucket: "edrekogbucket",
+        Name: "MarcMerill1.jpg"
+      }
     }
-  }
-};
-// rekognition.indexFaces(params, function (err, data) {
-//   if (err) console.log(err, err.stack); // an error occurred
-//   else console.log(data);           // successful response
-// });
+  };
+}
+
+
+rekognition.searchFacesByImage(params, function (err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else console.log(data);           // successful response
+});
 
 app.use(express.static('public'));
 
